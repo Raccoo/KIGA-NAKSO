a726eba86bed3e18238161943d82356c6b61c53a
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-05-14 09:56:37 +0900

    名前変更したのでreadmeとvagrantfileの編集

--- KIGA-NAKSO/menu/recipe_seach.php
+++ KIGA-NAKSO/menu/recipe_seach.php
@@ -3,8 +3,8 @@
 <html lang="ja">
 <head>
 	<title>レシピ</title>
-	<link href="../../css/bootstrap.min.css" rel="stylesheet">
-	<script src="../../js/bootstrap.min.js"></script>
+	<link href="../css/bootstrap.min.css" rel="stylesheet">
+	<script src="../js/bootstrap.min.js"></script>
 </head>
 <body>
 	<div class="input-group">
