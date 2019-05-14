0158332d95c9244a01f91c6d6a93b11f9c205381
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-05-14 11:55:04 +0900

    イメージファイルを追加

--- KIGA-NAKSO/menu/recipe_seach.php
+++ KIGA-NAKSO/menu/recipe_seach.php
@@ -10,11 +10,47 @@
 
 </head>
 <body>
-	<div class="row">
-		<div class="col-md-6">
-	<div class="input-group">
-		<input type="text" class="form-control" placeholder="レシピ検索">
-		<button>
-	<i class="fa fa-search"></i>
-	</button></div>
-</div></body>
+	<div class="container">
+		<div class="row justify-content-center">
+			<div class="col-10">
+				<div class="input-group">
+					<input type="text" class="form-control" placeholder="レシピ検索">
+					<button>
+						<i class="fa fa-search"></i>
+					</button>
+				</div>
+			</div>
+		<div class="row">
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+  					<img class="card-img-top" src="../img/omuraisu.jpg" alt="カードの画像" style="height: 14rem;">
+  					<div class="card-body">
+    					<h5 class="card-title">ドレスオムライス</h5>
+    					<p class="card-text" >鶏肉、たまねぎ、ピーマン...</p>
+    					<a href="#" class="btn btn-primary">材料を見る。</a>
+  					</div>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+  					<img class="card-img-top" src="../img/kare-.jpg" alt="カードの画像" style="height: 14rem;">
+  					<div class="card-body">
+   						 <h5 class="card-title">普通のカレー</h5>
+   						 <p class="card-text" >にんじん、じゃがいも、豚肉...</p>
+  						 <a href="#" class="btn btn-primary">材料を見る。</a>
+ 					 </div>
+				</div>
+			</div>
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+  					<img class="card-img-top" src="../img/monburan.jpg" alt="カードの画像" style="height: 14rem;">
+  					<div class="card-body">
+   						 <h5 class="card-title">ざるそばモンブラン</h5>
+   						 <p class="card-text" >栗、グラニュー糖、生クリーム</p>
+  						 <a href="#" class="btn btn-primary">材料を見る。</a>
+ 					 </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
