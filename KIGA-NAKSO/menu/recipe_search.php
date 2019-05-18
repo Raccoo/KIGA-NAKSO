bcd53d9b40b3c4a72f7cc18c7413d24e0d068006
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-05-19 04:37:20 +0900

    [Change] ディレクトリの構造を少し変更

--- KIGA-NAKSO/menu/recipe_search.php
+++ KIGA-NAKSO/menu/recipe_search.php
@@ -1,15 +1,8 @@
 <?php
-  require_once __DIR__ . '/../header.php';
+  require_once __DIR__ . '/../components/header.php';
   //require_once __DIR__ . '/../db/dbdata.php';
 ?>
 
-<head>
-	<title>レシピ</title>
-	<link href="../css/bootstrap.min.css" rel="stylesheet">
-	<script src="../js/bootstrap.min.js"></script>
-	<script defer src="../js/all.min.js"></script>
-</head>
-<body>
 	<div class="container">
 		<div class="row justify-content-center">
 			<div class="col-10">
@@ -68,5 +61,7 @@
 			</div>
 		</div>
 	</div>
-</body>
-</html>
+
+<?php
+  require_once __DIR__ . '/../components/footer.php';
+?>
