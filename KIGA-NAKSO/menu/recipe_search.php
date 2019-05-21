54888fcdd299cd8fd39e95ec774ca5cc6b55f87a
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-05-21 11:48:39 +0900

    [Change] change layout

--- KIGA-NAKSO/menu/recipe_search.php
+++ KIGA-NAKSO/menu/recipe_search.php
@@ -2,7 +2,8 @@
   require_once __DIR__ . '/../components/header.php';
 	require_once __DIR__ . '/../db/dbdata.php';
 	
-	$search_word = $_POST['search_word'];
+	$search_word = htmlspecialchars($_POST['search_word']);
+	$dbc = new DbData();
 ?>
 <head>
 	<title>レシピ検索ページ</title>
@@ -11,22 +12,19 @@
 	<div class="container">
 		<div class="row justify-content-center">
 			<?php
-			// $search_word
-			if ( !empty($search_word) ) {
-				echo '<div class="col-9 text-center alert alert-primary" role="alert"><a class="alert-link">「'
-					. $search_word . '」 </a>で検索しました</div>';
-				$dbc = new DbData();
-				$all_recipe_query = 'SELECT * FROM recipe WHERE r_name LIKE "%' . $search_word . '%"';
-				$result = $dbc->searchRecipe($all_recipe_query);
-			}
-			else {
-				$dbc = new DbData();
-				$all_recipe_query = "SELECT * FROM recipe";
-				$result = $dbc->searchRecipe($all_recipe_query);
-			}
-		?>
-		<br>
-		<div class="col-10">
+				// $search_word
+				if ( !empty($search_word) ) {
+					echo '<div class="col-9 text-center alert alert-primary" role="alert"><a class="alert-link">「'
+						. $search_word . '」 </a>で検索しました</div>';
+					$all_recipe_query = 'SELECT * FROM recipe WHERE r_name LIKE "%' . $search_word . '%"';
+					$result = $dbc->searchRecipe($all_recipe_query);
+				}
+				else {
+					$all_recipe_query = "SELECT * FROM recipe";
+					$result = $dbc->searchRecipe($all_recipe_query);
+				}
+			?>
+			<div class="col-10">
 				<form class="input-group" action="recipe_search.php" method="POST">
 					<input type="text" name="search_word" class="form-control" placeholder="レシピ検索">
 					<button type="submit">
@@ -35,45 +33,58 @@
 					</button>
 				</form>
 			</div>
+			</div>
+			<br>
+			<div class="row justify-content-center">
 			<?php
-
 				foreach($result as $row) {
 					echo '
-					<div class="col-4">
-						<div class="card" style="width: 18rem;">
-							<img class="card-img-top" src="' . $row['r_picture'] . '" alt="カードの画像" style="height: 14rem;">
-							<div class="card-body">
-								<h5 class="card-title">' . $row['r_name'] . '</h5>
-								<p class="card-text">';
+						<div class="col-4">
+							<div class="card" style="width: 18rem;">
+								<img class="card-img-top" src="' . $row['r_picture'] . '" alt="カードの画像" style="height: 14rem;">
+								<div class="card-body">
+									<h5 class="card-title">' . $row['r_name'] . '</h5>
+									<p class="card-text">';
+						
+						// Get the ingredients used in the recipe from db.
+						$foods_name_query = 'SELECT DISTINCT master_food.f_name 
+							FROM recipe_food, recipe, master_food 
+							WHERE recipe_food.r_id = '
+							. $row['r_id'] . 
+							' AND recipe_food.f_id = master_food.f_id';
 					
-					// Get the ingredients used in the recipe from db.
-					$foods_name_query = 'SELECT DISTINCT master_food.f_name 
-						FROM recipe_food, recipe, master_food 
-						WHERE recipe_food.r_id = '
-						 . $row['r_id'] . 
-						' AND recipe_food.f_id = master_food.f_id';
-				
-					$items = $dbc->searchRecipe($foods_name_query);
-					
-					// process to display ingredients.
-					$counter = 0;
-					foreach($items as $a) {
-						if( $counter >= 3 ){
-							echo '...';
-							break;
+						$items = $dbc->searchRecipe($foods_name_query);
+						
+						// process to display ingredients.
+						$counter = 0;
+						foreach($items as $a) {
+							if( $counter >= 3 ){
+								echo '...';
+								break;
+							}
+							echo $a['f_name'] . '、';
+							$counter += 1;
 						}
-						echo $a['f_name'] . '<br>';
-						$counter += 1;
-					}
 
-					echo'	
-								</p>
-								<a href="#" class="btn btn-primary">材料を見る</a>
+						echo'	
+									</p>
+									<a href="#" class="btn btn-primary">材料を見る</a>
+								</div>
 							</div>
+						</div>';
+					}
+				?>
+				<div class="col-4">
+					<div class="card" style="width: 18rem;">
+							<img class="card-img-top" src="../img/monburan.jpg" alt="カードの画像" style="height: 14rem;">
+							<div class="card-body">
+								<h5 class="card-title">ざるそばモンブラン</h5>
+								<p class="card-text" >栗、グラニュー糖、生クリーム</p>
+								<a href="#" class="btn btn-primary">材料を見る</a>
 						</div>
-					</div>';
-				}
-			?>
+					</div>
+				</div>
+			</div>
 		</div>
 	</div>
 
