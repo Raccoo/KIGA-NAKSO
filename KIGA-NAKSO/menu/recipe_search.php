2e75e7c7651d881f21c67a6da92d1de2532ca9f3
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-05-21 10:14:41 +0900

    Integration

--- KIGA-NAKSO/menu/recipe_search.php
+++ KIGA-NAKSO/menu/recipe_search.php
@@ -17,54 +17,6 @@
 					</form>
 				<!-- </div> -->
 			</div>
-<<<<<<< HEAD
-		<div class="row">
-			<div class="col-4">
-				<?php
-				//echo 'aaa';
-					//$dbc = new mysqli('localhost','kiga','nakso','kiga_nakso'); //or die('Error connectiong to mysql server');
-					//mysqli_set_charaset($dbc,'utf8');
-					//$query = "SELECT * FROM recipe";
-					//$result = mysqli_query($dbc,$query); //or die('Error querying database');
-					//while ($row = mysql_fetch_array($result)){
-					//	echo 'vvvv';
-					//	$recipe_name = $row['r_name'];
-					//	echo $recipe_name;
-					//}
-				?>
-				<?php
-				//echo 'bbb';
-				?>
-				<div class="card" style="width: 18rem;">
-  					<img class="card-img-top" src="../img/omuraisu.jpg" alt="カードの画像" style="height: 14rem;">
-  					<div class="card-body">
-    					<h5 class="card-title">ドレスオムライス</h5>
-    					<p class="card-text" >鶏肉、たまねぎ、ピーマン...</p>
-    					<a href="#" class="btn btn-primary">材料を見る。</a>
-  					</div>
-				</div>
-			</div>
-			<div czlass="col-4">
-				<div class="card" style="width: 18rem;">
-  					<img class="card-img-top" src="../img/kare-.jpg" alt="カードの画像" style="height: 14rem;">
-  					<div class="card-body">
-   						 <h5 class="card-title">普通のカレー</h5>
-   						 <p class="card-text" >にんじん、じゃがいも、豚肉...</p>
-  						 <a href="#" class="btn btn-primary">材料を見る。</a>
- 					 </div>
-				</div>
-			</div>
-			<div class="col-4">
-				<div class="card" style="width: 18rem;">
-  					<img class="card-img-top" src="../img/monburan.jpg" alt="カードの画像" style="height: 14rem;">
-  					<div class="card-body">
-   						 <h5 class="card-title">ざるそばモンブラン</h5>
-   						 <p class="card-text" >栗、グラニュー糖、生クリーム</p>
-  						 <a href="#" class="btn btn-primary">材料を見る。</a>
- 					 </div>
-				</div>
-			</div>
-=======
 			<?php
 				$dbc = new DbData();
 				$all_recipe_query = "SELECT * FROM recipe";
@@ -107,7 +59,6 @@
 					</div>';
 				}
 			?>
->>>>>>> 263b2b57fd785b627b7f984fbcafb97df865c1c4
 		</div>
 	</div>
 
