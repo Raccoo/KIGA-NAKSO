1fdf633e7762c98749ec9ec24a0b900f3fb5111f
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-05-21 13:51:06 +0900

    [Add] レシピページ作成

--- KIGA-NAKSO/menu/recipe_view.php
+++ KIGA-NAKSO/menu/recipe_view.php
@@ -19,7 +19,26 @@
 </head>
 <body>
 	<div class="container">
-    <h1><?php echo $recipe['r_name']; ?></h1>
+    <?php 
+      echo '<h1>' . $recipe['r_name'] . '</h1>';
+      echo '<image src="' . $recipe['r_picture'] . '">';
+      
+      // Get the ingredients used in the recipe from db.
+			$foods_name_query = 'SELECT DISTINCT master_food.f_name 
+			FROM recipe_food, recipe, master_food 
+			WHERE recipe_food.r_id = '
+				. $recipe['r_id'] . 
+			  ' AND recipe_food.f_id = master_food.f_id';
+
+			$items = $dbc->searchRecipe($foods_name_query);
+
+			// process to display ingredients.
+			foreach($items as $a) {
+				echo ' * ' . $a['f_name'] . ' : ' . $a['f_volume'] . '<br>';
+			}
+    
+      echo '<image src="' . $recipe['cuisine'] . '">';
+    ?>
   </div>
 <?php
   require_once __DIR__ . '/../components/footer.php'
