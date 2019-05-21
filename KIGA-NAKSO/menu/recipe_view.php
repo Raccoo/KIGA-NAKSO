605c50e3ccb8e0aa536d1d39f7dfd4fae33cf70b
kazu_mask <mail.kazu.mask@gmail.com>
2019-05-21 13:37:52 +0900

    recipe_view ぴょろん

---
+++ KIGA-NAKSO/menu/recipe_view.php
@@ -0,0 +1,13 @@
+<?php
+require_once __DIR__ . '/../components/header.php';
+require_once __DIR__ . '/../db/dbdata.php';
+
+$dbc = new DbData();
+//PHP学習・DB出来次第、処理を書く
+?>
+
+
+
+<?php
+require_once __DIR__ . '/../components/footer.php';
+?>
