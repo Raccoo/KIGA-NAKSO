da4850c997c9fca6da1a99bc0b81eb578ee21aed
various3211workd <kd1229568@st.kobedenshi.ac.jp>
2019-05-24 13:45:10 +0900

    [Change] index.php layout changed

--- KIGA-NAKSO/index.php
+++ KIGA-NAKSO/index.php
@@ -10,12 +10,13 @@
 		<script defer src="js/all.min.js"></script>
 	</head>
 	<body>
-
+		<br><br>
 		<div class="text-center">
 			<h1><img id="blah" src="http://placehold.it/150" alt="your image">KIGA-NAKSO</h1><br>
 
 			<img src="img/SDGs.jpg" class="img-responsive center-block">
 		</div>
+		<br><br>
 	</body>
 </html>
 
