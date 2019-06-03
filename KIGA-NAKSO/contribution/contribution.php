d1fe15abdc9ac77082288dd811ec2cced3b3cd12
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-06-03 13:25:35 +0900

    [Add]contribution folder & contribution.php

---
+++ KIGA-NAKSO/contribution/contribution.php
@@ -0,0 +1,84 @@
+<?php
+	require_once __DIR__ . '/../components/header.php';
+	require_once __DIR__ . '/../db/dbdata.php';
+?>
+<head>
+	<title>貢献度</title>
+</head>
+<body><br>
+	<h3 class="text-center"><a href="#"><</a>  2019-06  <a href="#">></a></h3>
+	<div class="row">
+		<div class="container">
+			<ul class="nav nav-tabs">
+				<li class="nav-item">
+					<a href="#tab1" class="nav-link active" data-toggle="tab">肉類</a>			
+				</li>
+				<li class="nav-item">
+					<a href="#tab2" class="nav-link" data-toggle="tab">野菜・フルーツ</a>
+				</li>
+				<li class="nav-item">
+					<a href="#tab3" class="nav-link" data-toggle="tab">穀物類</a>
+				</li>
+				<li class="nav-item">
+					<a href="#tab4" class="nav-link" data-toggle="tab">水産類</a>
+				</li>
+			</ul>
+		</div>
+	</div>
+	<div class="p-3">
+		<div class="tab-content">
+			<div id="tab1" class="tab-pane active">
+				<!--肉類-->
+				<div class="container">
+					<div class="row">
+						<div class="col-6">
+							無駄ァ
+						</div>
+						<div class="col-6">
+							現在の冷蔵庫の中身
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="tab2" class="tab-pane">
+				<!--野菜・フルーツ-->
+				<div class="container">
+					<div class="row">
+						<div class="col-6">
+							無駄ァ
+						</div>
+						<div class="col-6">
+							現在の冷蔵庫の中身
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="tab3" class="tab-pane">
+				<!--穀物類-->
+				<div class="container">
+					<div class="row">
+						<div class="col-6">
+							無駄ァ
+						</div>
+						<div class="col-6">
+							現在の冷蔵庫の中身
+						</div>
+					</div>
+				</div>
+			</div>
+			<div id="tab4" class="tab-pane">
+				<!--水産類-->
+				<div class="container">
+					<div class="row">
+						<div class="col-6">
+							無駄ァ
+						</div>
+						<div class="col-6">
+							現在の冷蔵庫の中身
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+</body>
