a4e1e1d9c845f0cd77bb7e4c28d01f26a7fffeee
Raccoo <kd1196317@st.kobedenshi.ac.jp>
2019-06-18 10:49:14 +0900

    [Change]icon change

--- KIGA-NAKSO/index.php
+++ KIGA-NAKSO/index.php
@@ -64,7 +64,7 @@
 	<body>
 		<br><br>
 		<div class="text-center">
-			<h1><img id="blah" src="http://placehold.it/150" alt="your image">KIGA-NAKSO</h1><br>
+			<h1><img id="blah" src="img/kiganakso_red.png" alt="your image" width="100px" height="100px">KIGA-NAKSO</h1><br>
 
 			<img src="img/SDGs.jpg" class="img-responsive center-block">
 		</div>
